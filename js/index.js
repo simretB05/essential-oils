36b97a77043d657128e189429f1d2f500129247e
simretB05 <happysimret@gmai.com>
2022-05-16 02:00:19 -0600

    add animation to nav bar and nav list

--- js/index.js
+++ js/index.js
@@ -1,22 +1,65 @@
 // targets the humburger menue and the button which contains it
 
-const toggleBtn = document.querySelector('.header__toggle')
-const navBtn = document.querySelector('.navbar')
-const header = document.querySelector('.header')
-let toggleOpen = false;
-toggleBtn.addEventListener('click', () => {
-    if (!toggleOpen) {
-        toggleBtn.classList.remove('open');
-        navBtn.classList.remove('move-in')
-        header.classList.remove('overlay');
+// const toggleBtn = document.querySelector('.header__toggle')
+// const navBtn = document.querySelector('.navbar')
+// const header = document.querySelector('.header')
+// let toggleOpen = false;
+// toggleBtn.addEventListener('click', () => {
+//     if (!toggleOpen) {
+//   toggleBtn.classList.remove('open');
+//     navBtn.classList.remove('move-in')
+//     header.classList.remove('overlay');
 
-        toggleOpen = true;
-    } else {
+//     toggleOpen = true;
+//     } else {
+//      
 
-        navBtn.classList.add('move-in')
-        navBtn.classList.remove('move-out')
-        toggleBtn.classList.add('open');
-        header.classList.toggle('overlay');
-        toggleOpen = false;
-    }
-});
+// navBtn.classList.add('move-in')
+// navBtn.classList.remove('move-out')
+// toggleBtn.classList.add('open');
+// header.classList.toggle('overlay');
+// toggleOpen = false;
+//     }
+// });
+// another cool way
+const toggle = () => {
+    const toggleBtn = document.querySelector('.header__toggle-burger');
+    const toggleBurger = document.querySelector('.header__toggle')
+    const navBtn = document.querySelector('.navbar');
+    const header = document.querySelector('.header');
+    const navLinks = document.querySelectorAll('.navbar__list li');
+    let toggleOpen = false;
+
+
+    //toggle navbars
+    toggleBtn.addEventListener('click', () => {
+        navBtn.classList.toggle('navToggle-active')
+
+
+        if (!toggleOpen) {
+            toggleBurger.classList.add('open');
+            header.classList.toggle('overlay');
+            toggleOpen = true;
+
+        } else {
+
+            toggleBurger.classList.remove('open');
+            header.classList.remove('overlay');
+            toggleOpen = false;
+        }
+        // toggleBtn.classList.remove('open');
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navLinkFade .5s ease-in forwards ${index / 10 + 1}s`
+            }
+
+        })
+
+    });
+
+}
+
+
+toggle();
